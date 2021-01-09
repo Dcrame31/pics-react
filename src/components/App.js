290860cdc30ef5cca9976c76dfa3d01054d89ee5
Diane Crame <dcrame@icloud.com>
2021-01-08 16:54:20 -0800

    add search bar and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 const App = () => {
-    return <div>
-        App
+    return <div className="ui container" style={{marginTop: '10px'}}>
+        <SearchBar />
     </div>
 };
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,3 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                    <label>Image Search</label>
+                    <input type="text" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
