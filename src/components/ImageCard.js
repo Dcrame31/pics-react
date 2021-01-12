82d14218d51779870b541f921fd8cbf7645b18b8
Diane Crame <dcrame@icloud.com>
2021-01-11 16:42:38 -0800

    adjust imageCard grid

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { spans: 0};
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans)
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height / 10 + 1)
+
+        this.setState({ spans })
+    }
+    
     render() {
+        const {description, urls} = this.props.image
         return (
-            <div>
-                <img alt={this.props.image.description}
-                src={this.props.image.urls.regular}
+            <div style={{ gridRowEnd: `span ${this.state.spans}`}}>
+                <img 
+                ref={this.imageRef}
+                alt={description}
+                src={urls.regular}
                 />
             </div>
         )
